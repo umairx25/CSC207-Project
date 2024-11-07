907f2380e24da3642bbeddb813d1171661fb99e8
Umair Arham <81112079+umairx25@users.noreply.github.com>
2024-11-07 15:39:15 -0500

    Added starter code from Lab 5

---
+++ src/main/java/use_case/logout/LogoutInputData.java
@@ -0,0 +1,18 @@
+package use_case.logout;
+
+/**
+ * The Input Data for the Logout Use Case.
+ */
+public class LogoutInputData {
+
+    private String username;
+
+    public LogoutInputData(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+}
