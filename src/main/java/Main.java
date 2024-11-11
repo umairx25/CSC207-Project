db33e36f0b4cba222440063c583d685a35f07ccf
Umair Arham <umairarham@umairs-air.home>
2024-11-10 22:50:41 -0500

    Added login and signin functionality using firebase

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,17 @@
-// src/Main.java
-import ui.StockSimulatorFrame;
+import com.google.firebase.cloud.FirestoreClient;
+import frameworks_driver.Login;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Main {
-    public static void main(String[] args) {
-        new StockSimulatorFrame();
+
+    // Sample login/signup use case
+    public static void main(String[] args) throws Exception {
+        Dotenv dotenv = Dotenv.load();
+        frameworks_driver.NewUser user = new frameworks_driver.NewUser("aa230@yahoo.com", "abcdefg", "user112");
+        frameworks_driver.NewUser.initialize_firebase(dotenv.get("FIREBASE_INFO"));
+        user.signup(FirestoreClient.getFirestore());
+        String id = Login.login("aa230@yahoo.com", "abcdefghi");
+        Login.initialize_firebase(dotenv.get("FIREBASE_INFO"));
+        System.out.println(Login.verify_login(id));
     }
 }
