fdb0c07003b7f1412576cf893d01d759d19a20c7
YaseenSadat <yaseensadat2@gmail.com>
2024-11-11 21:46:17 -0500

    fixing git errrors

--- src/main/java/use_case/Main.java
+++ src/main/java/use_case/Main.java
@@ -7,7 +7,6 @@ import ui.MainFrame;
 
 public class Main {
 
-    // Sample login/signup use case
     public static void main(String[] args) throws Exception {
         Dotenv dotenv = Dotenv.load();
         new MainFrame();
