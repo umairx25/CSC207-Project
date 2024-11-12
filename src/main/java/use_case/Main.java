3b127d697327bed8d73ebe5bbf335245228377e6
YaseenSadat <yaseensadat2@gmail.com>
2024-11-11 21:39:34 -0500

    fixing git errors caused by pull

--- src/main/java/use_case/Main.java
+++ src/main/java/use_case/Main.java
@@ -10,13 +10,7 @@ public class Main {
     // Sample login/signup use case
     public static void main(String[] args) throws Exception {
         Dotenv dotenv = Dotenv.load();
-//        frameworks_driver.NewUser user = new frameworks_driver.NewUser("aa230@yahoo.com", "abcdefg", "user112");
-//        frameworks_driver.NewUser.initialize_firebase(dotenv.get("FIREBASE_INFO"));
-//        user.signup(FirestoreClient.getFirestore());
-//        String id = Login.login("aa230@yahoo.com", "abcdefghi");
         new MainFrame();
         Login.initialize_firebase(dotenv.get("FIREBASE_INFO"));
-        //System.out.println(Login.preexisting_email("aa23000@yahoo.com"));
-//        System.out.println(Login.verify_login(id));
     }
 }
