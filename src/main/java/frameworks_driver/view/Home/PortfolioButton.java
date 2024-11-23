7e216352f276d64471f6bb53f4d54339c82d132c
aneeqmuh <aneeqanno@gmail.com>
2024-11-23 15:13:00 -0500

    Home View Frontend With CA 2

--- src/main/java/frameworks_driver/view/Home/PortfolioButton.java
+++ src/main/java/frameworks_driver/view/Home/PortfolioButton.java
@@ -1,5 +1,7 @@
 package frameworks_driver.view.Home;
 
+import frameworks_driver.view.Home.HoverEffectUtility;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -13,20 +15,6 @@ public class PortfolioButton extends JButton {
         setForeground(new Color(6, 26, 64)); // Set font color to dark blue
         setOpaque(true); // Ensure the background is painted
         setBorder(BorderFactory.createLineBorder(new Color(6, 26, 64), 2)); // Dark blue border
-        addHoverEffect(); // Add hover effects
-    }
-
-    private void addHoverEffect() {
-        addMouseListener(new java.awt.event.MouseAdapter() {
-            @Override
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                setBackground(new Color(220, 220, 220)); // Light gray on hover
-            }
-
-            @Override
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                setBackground(Color.WHITE); // Revert to white on exit
-            }
-        });
+        HoverEffectUtility.applyHoverEffect(this); // Add hover effect
     }
 }
