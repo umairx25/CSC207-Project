2c8473452fdd02f4361fc498ab5f6d883903a95f
aneeqmuh <aneeqanno@gmail.com>
2024-11-27 19:20:06 -0500

    Log in CA and Home page front end

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,4 +1,26 @@
 package entity;
 
 public class User {
+    private final String name;
+    private final String email;
+    private final String password; // Optional: Can be null
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
